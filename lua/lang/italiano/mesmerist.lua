c4c0c7007fef251bc7830f5caba19d46a7a8257e
ThePlatinumGhost <36534723+ThePlatinumGhost@users.noreply.github.com>
2020-07-11 14:38:53 +0200

    Create italian

---
+++ lua/lang/italiano/mesmerist.lua
@@ -0,0 +1,23 @@
+L = LANG.GetLanguageTableReference("italiano")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[MESMERIST.name] = "Ipnotista"
+L[MESMERIST.defaultTeam] = "TEAM Traditori"
+L["info_popup_" .. MESMERIST.name] = [[Ora è il tuo turno prova a prendere qualche giocatore morto per combattere al tuo fianco!]]
+L["body_found_" .. MESMERIST.abbr] = "Loro erano Ipnotisti!"
+L["search_role_" .. MESMERIST.abbr] = "Questa persona era Ipnotista!"
+L["target_" .. MESMERIST.name] = "Ipnostista"
+L["ttt2_desc_" .. MESMERIST.name] = [[L'Ipnotista è un traditore che puo rianimare i morti, Lavandogli il cervello e facendoli diventare Traditori!]]
+
+L["revived_by_mesmerist"] = "Sei stato rianimato da {name} come Traditore. Preparati!"
+L["mesdefi_hold_key_to_revive"] = "Tieni premuto [{key}] per rianimare il giocatore come traditore"
+L["mesdefi_revive_progress"] = "Tempo rimasto: {time} secondi"
+L["mesdefi_charging"] = "il defribillatore si sta ricaricando, per favore aspetta"
+L["mesdefi_player_already_reviving"] = "Il giocatore sta gia venendo rianimato"
+L["mesdefi_error_no_space"] = "Non c'è abbastanza spazio per rianimare."
+L["mesdefi_error_too_fast"] = "il defribillatore si sta ricaricando, per favore aspetta"
+L["mesdefi_error_lost_target"] = "Hai perso il tuo bersaglio. Per favore riprova."
+L["mesdefi_error_no_valid_ply"] = "Non puoi rianimare questo giocatore perchè non è più valido."
+L["mesdefi_error_already_reviving"] = "Non puoi rianimare questo giocatore perche sta già venendo rianimato"
+L["mesdefi_error_failed"] = "Tentativo di rianimazione fallito. Per favore riprova."
+L["mesdefi_error_thrall"] = "Non puoi rianimare uno Schiavo!"
---
+++ lua/lang/italiano/thrall.lua
@@ -0,0 +1,10 @@
+L = LANG.GetLanguageTableReference("italiano")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[THRALL.name] = "Schiavo"
+L[THRALL.defaultTeam] = "TEAM Traditori"
+L["info_popup_" .. THRALL.name] = [[Sei stato rianimato come traditore!]]
+L["body_found_" .. THRALL.abbr] = "Loro erano Schiavi!"
+L["search_role_" .. THRALL.abbr] = "Questa persona era uno Schiavo!"
+L["target_" .. THRALL.name] = "Schiavo"
+L["ttt2_desc_" .. THRALL.name] = [[Lo Schiavo è un traditore che è stato rianimato e convertito dall'ipnotista!]]
