9a2a47719e9bbc923f266da37bc388b15540747d
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-09-09 23:32:47 -0400

    Grammar fix

--- lua/terrortown/lang/english/mesmerist.lua
+++ lua/terrortown/lang/english/mesmerist.lua
@@ -9,7 +9,7 @@ L["search_role_" .. MESMERIST.abbr] = "This person was a Mesmerist!"
 L["target_" .. MESMERIST.name] = "Mesmerist"
 L["ttt2_desc_" .. MESMERIST.name] = [[The Mesmerist is a traitor who can revive the dead, brainwashing them into becomeing traitors!]]
 
-L["revived_by_mesmerist"] = "You are revived by {name} as a traitor. Prepare yourself!"
+L["revived_by_mesmerist"] = "You are being revived by {name} as a traitor. Prepare yourself!"
 L["mesdefi_hold_key_to_revive"] = "Hold [{key}] to revive player as a traitor"
 L["mesdefi_revive_progress"] = "Time left: {time}s"
 L["mesdefi_charging"] = "Defibrillator is recharging, please wait"
