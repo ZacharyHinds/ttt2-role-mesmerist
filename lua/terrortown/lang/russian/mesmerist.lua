ef5f4d2cea99060cbcfdfb4ace89fe901a5a242f
Blueberryy <igory.ygr200@gmail.com>
2020-09-27 19:03:52 +0500

    translation

---
+++ lua/terrortown/lang/russian/mesmerist.lua
@@ -0,0 +1,23 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[MESMERIST.name] = "Месмерист"
+L[MESMERIST.defaultTeam] = "Команда месмеристов"
+L["info_popup_" .. MESMERIST.name] = [[Теперь твоя очередь! Попытайтесь заставить некоторых мёртвых игроков сражаться на вашей стороне!]]
+L["body_found_" .. MESMERIST.abbr] = "Он был месмеристом!"
+L["search_role_" .. MESMERIST.abbr] = "Этот человек был месмеристом!"
+L["target_" .. MESMERIST.name] = "Месмерист"
+L["ttt2_desc_" .. MESMERIST.name] = [[Месмерист - предатель, который может воскрешать мёртвых, промывая им мозги, превращая их в предателей!]]
+
+L["revived_by_mesmerist"] = "{name} возрождает вас как предателя. Приготовься!"
+L["mesdefi_hold_key_to_revive"] = "Удерживайте [{key}], чтобы оживить игрока как предателя"
+L["mesdefi_revive_progress"] = "Осталось времени: {time}с"
+L["mesdefi_charging"] = "Дефибриллятор заряжается, подождите."
+L["mesdefi_player_already_reviving"] = "Игрок уже оживает"
+L["mesdefi_error_no_space"] = "Недостаточно места для этой попытки возрождения."
+L["mesdefi_error_too_fast"] = "Дефибриллятор заряжается. Пожалуйста, подождите."
+L["mesdefi_error_lost_target"] = "Вы потеряли свою цель. Пожалуйста, попробуйте ещё раз."
+L["mesdefi_error_no_valid_ply"] = "Вы не можете оживить этого игрока, так как он больше не действителен."
+L["mesdefi_error_already_reviving"] = "Вы не можете оживить этого игрока, так как он уже оживает."
+L["mesdefi_error_failed"] = "Попытка возрождения не удалась. Пожалуйста, попробуйте ещё раз."
+L["mesdefi_error_thrall"] = "Вы не можете оживить раба!"
---
+++ lua/terrortown/lang/russian/thrall.lua
@@ -0,0 +1,10 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[THRALL.name] = "Раб"
+L[THRALL.defaultTeam] = "Команда рабов"
+L["info_popup_" .. THRALL.name] = [[Вы были возрождены предателем!]]
+L["body_found_" .. THRALL.abbr] = "Он был рабом!"
+L["search_role_" .. THRALL.abbr] = "Этот человек был рабом!"
+L["target_" .. THRALL.name] = "Раб"
+L["ttt2_desc_" .. THRALL.name] = [[Раб - предатель, возрождённый и обращённый месмеристом!]]
